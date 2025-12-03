c0af0d93b79afd997ea3ec930e57e34767293fd2
Prashansha Neupane <neupane.prashansha2005@gmail.com>
2025-12-03 19:46:44 +0545

    get/post

--- ExpressJS/form.js
+++ ExpressJS/form.js
@@ -1,16 +1,45 @@
+// const express = require("express");
+// const app = express();
+// const port = 5000;
+
+// // to read form data
+
+// app.use(express.urlencoded({ extended: true }));
+
+// // post route
+// app.post("/contact", (req, res) => {
+//   const { name, email } = req.body;
+//   res.send(`Received: Name = ${name}, Email = ${email}`);
+// });
+
+// // Start server
+// app.listen(port, () => {
+//   console.log(`Server running at http://localhost:${port}`);
+// });
+
+// GET Request with Query Params and URL Params
 const express = require("express");
 const app = express();
 const port = 5000;
 
-// to read form data
-
-app.use(express.urlencoded({ extended: true }));
-
-// post route
-app.post("/contact", (req, res) => {
-  const { name, email } = req.body;
-  res.send(`Received: Name = ${name}, Email = ${email}`);
+// Simple GET
+app.get("/info", (req, res) => {
+  res.send("Server is running.");
 });
+
+// GET with Query Params
+app.get("/user", (req, res) => {
+  const { name, age } = req.query;
+
+  res.send(`Query Received\nName: ${name}\nAge: ${age}`);
+});
+
+// // GET with URL Params
+// app.get("/product/:id", (req, res) => {
+//   const productId = req.params.id;
+
+//   res.send(`Product ID Received: ${productId}`);
+// });
 
 // Start server
 app.listen(port, () => {
